54efc27fe7e238f7ddc701a8ac45546fb7c0cebb
4ofl <ly4ofl@outlook.com>
2025-08-26 13:30:57 +0530

    og meta update

--- api/[id].js
+++ api/[id].js
@@ -26,20 +26,32 @@ export default async function handler(req, res) {
 
 	res.setHeader("Content-Type", "text/html");
 	res.send(`
-    <!doctype html>
-    <html>
-      <head>
-        <title>${title}</title>
-        <meta property="og:title" content="${title}" />
-        <meta property="og:description" content="${desc}" />
-        <meta property="og:url" content="https://bin.mxrn.lol/${id}" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary" />
-      </head>
-      <body>
-        <div id="root"></div>
-        <script src="/static/js/bundle.js"></script>
-      </body>
-    </html>
+		<!doctype html>
+		<html lang="en">
+		<head>
+			<meta charset="utf-8" />
+			<title>${title}</title>
+
+			<!-- og tags -->
+			<meta property="og:title" content="${title} | bin.mxrn.lol" />
+			<meta property="og:description" content="${desc}" />
+			<meta property="og:url" content="https://bin.mxrn.lol/${id}" />
+			<meta property="og:type" content="website" />
+			<meta property="og:image" content="https://bin.mxrn.lol/icon.png" />
+
+			<!-- twitter card -->
+			<meta name="twitter:card" content="summary_large_image" />
+			<meta name="twitter:title" content="${title}" />
+			<meta name="twitter:description" content="${desc}" />
+			<meta name="twitter:image" content="https://bin.mxrn.lol/icon.png" />
+
+			<!-- favicon -->
+			<link rel="icon" href="/icon.png" />
+		</head>
+		<body>
+			<div id="root"></div>
+			<script src="/static/js/bundle.js"></script>
+		</body>
+		</html>
   `);
 }
